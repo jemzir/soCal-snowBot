b6bcecd24f1e5f784680f4e4dcc741592b72c9a8
jemzir <nghiemj23@gmail.com>
2022-12-30 17:15:59 -0800

    continue merge conflicts and naming conventions

--- SnowBotMain.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-import config
-
-client = tweepy.Client(consumer_key=config.API_KEY,
-                       consumer_secret=config.API_SECRET,
-                       access_token=config.ACCESS_TOKEN,
-                       access_token_secret=config.ACCESS_TOKEN_SECRET)
-
-response = client.create_tweet()
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import tweepy
+import config
+
+client = tweepy.Client(consumer_key=config.API_KEY,
+                       consumer_secret=config.API_SECRET,
+                       access_token=config.ACCESS_TOKEN,
+                       access_token_secret=config.ACCESS_TOKEN_SECRET)
+
+response = client.create_tweet()
+
 # import in tweetpy
 # https://www.youtube.com/watch?v=2UBcRiddwAo <-- use this vid
 
